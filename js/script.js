3c48058e9570a75fcfdd064c85c2e7290d73e5e7
Jinlong Yang <jinlong25@gmail.com>
2019-06-10 10:58:21 -0700

    test smaller shield

--- js/script.js
+++ js/script.js
@@ -5,16 +5,16 @@ d3.csv(dataUrl).then(function(data) {
 	var nested = d3.nest()
   	.key(function(d) { return d.date; })
   	.entries(data);
-	
+
 	console.log(nested);
-	
+
 	var cards = d3.select('#gameResults')
 		.selectAll('div.card')
 		.data(nested.reverse())
 		.enter()
 		.append('div')
 		.attr('class', 'card');
-		
+
 	var cardHeaders = cards.append('div')
 		.attr('class', 'card-header')
 		.attr('id', function(d, i) {
@@ -22,18 +22,18 @@ d3.csv(dataUrl).then(function(data) {
 		})
 		.append('h5')
 		.attr('class', 'mb-0 text-center text-primary')
-		// .append('button')
-		// .attr('class', 'btn btn-link')
-		// .attr('type', 'button')
-		// .attr('data-toggle', 'collapse')
-		// .attr('data-target', function(d, i) {
-		// 	return '#collapseGameDay' + (nested.length - i);
-		// })
+		.append('button')
+		.attr('class', 'btn btn-link')
+		.attr('type', 'button')
+		.attr('data-toggle', 'collapse')
+		.attr('data-target', function(d, i) {
+			return '#collapseGameDay' + (nested.length - i);
+		})
 		.attr('aria-expanded', 'true')
 		.html(function(d, i) {
 			return 'Game Day ' + (nested.length - i) + ' - ' + d.key;
 		});
-		
+
 	var cardBodies = cards.append('div')
 		.attr('class', 'collapse show')
 		.attr('id', function(d, i) {
@@ -42,15 +42,15 @@ d3.csv(dataUrl).then(function(data) {
 		.append('div')
 		.attr('class', 'card-body text-center')
 		.append('table')
-		.attr('class', 'table table-sm') //table-responsive 
+		.attr('class', 'table table-sm') //table-responsive
 		.append('tbody');
-		
+
 	var games = cardBodies.selectAll('td')
 		.data(function(d) { return d.values; })
 		.enter()
 		.append('tr')
 		.attr('class', 'd-flex table-borderless');
-	
+
 	games.append('td')
 		.attr('class', function(g) {
 			if (parseInt(g.home_goal) > parseInt(g.away_goal)) {
@@ -59,13 +59,14 @@ d3.csv(dataUrl).then(function(data) {
 				return 'text-right col-5';
 			}
 		})
-		.html(function(g) { return g.home + '<span><img src="https://github.com/jinlong25/amgen-soccer-league/raw/master/img/' + g.home + '.png"></span>'; });
-		
-	
+		// .html(function(g) { return g.home + '<span><img src="https://github.com/jinlong25/amgen-soccer-league/raw/master/img/' + g.home + '.png"></span>'; });
+		.html(function(g) { return g.home + '<span><img src="/img/' + g.home + '.png"></span>'; });
+
+
 	games.append('td')
 		.attr('class', 'text-center col-2')
 		.html(function(g) { return g.home_goal + ':' + g.away_goal; });
-	
+
 	games.append('td')
 		.attr('class', function(g) {
 			if (parseInt(g.home_goal) < parseInt(g.away_goal)) {
@@ -74,18 +75,19 @@ d3.csv(dataUrl).then(function(data) {
 				return 'text-left col-5';
 			}
 		})
-		.html(function(g) { return '<span><img src="https://github.com/jinlong25/amgen-soccer-league/raw/master/img/' + g.away + '.png"></span>' + g.away; });
-		
+		// .html(function(g) { return '<span><img src="https://github.com/jinlong25/amgen-soccer-league/raw/master/img/' + g.away + '.png"></span>' + g.away; });
+		.html(function(g) { return '<span><img src="/img/' + g.away + '.png"></span>' + g.away; });
+
 		// .html(function(d) {
 		// 	d.values.forEach
 		// 	console.log(d);
-		// 	return d.values[0].home 
+		// 	return d.values[0].home
 		// 		+ ' '
-		// 		+ d.values[0].home_goal 
+		// 		+ d.values[0].home_goal
 		// 		+ ':'
 		// 		+ d.values[0].away_goal
 		// 		+ ' '
 		// 		+ d.values[0].away;
 		// });
-		
+
 });
