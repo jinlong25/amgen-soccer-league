b1fac3a5e62279e99ca2b9431b1ec4be8f6927fd
jinlong25 <jinlong25@gmail.com>
2019-06-09 23:12:48 -0700

    alpha version

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ d3.csv(dataUrl).then(function(data) {
 		.append('div')
 		.attr('class', 'card');
 		
-	cards.append('div')
+	var cardHeaders = cards.append('div')
 		.attr('class', 'card-header')
 		.attr('id', function(d, i) {
 			return 'gameDay' + (nested.length - i);
@@ -34,26 +34,58 @@ d3.csv(dataUrl).then(function(data) {
 			return 'Game Day ' + (nested.length - i) + ' - ' + d.key;
 		});
 		
-	cards.append('div')
+	var cardBodies = cards.append('div')
 		.attr('class', 'collapse show')
 		.attr('id', function(d, i) {
 			return 'collapseGameDay' + (nested.length - i);
 		})
 		.append('div')
 		.attr('class', 'card-body text-center')
+		.append('table')
+		.attr('class', 'table table-sm') //table-responsive 
+		.append('tbody');
 		
+	var games = cardBodies.selectAll('td')
+		.data(function(d) { return d.values; })
+		.enter()
+		.append('tr')
+		.attr('class', 'd-flex table-borderless');
+	
+	games.append('td')
+		.attr('class', function(g) {
+			if (parseInt(g.home_goal) > parseInt(g.away_goal)) {
+				return 'text-right col-5 font-weight-bold';
+			} else {
+				return 'text-right col-5';
+			}
+		})
+		.html(function(g) { return g.home + '<span><img src="/img/' + g.home + '.png"></span>'; });
 		
+	
+	games.append('td')
+		.attr('class', 'text-center col-2')
+		.html(function(g) { return g.home_goal + ':' + g.away_goal; });
+	
+	games.append('td')
+		.attr('class', function(g) {
+			if (parseInt(g.home_goal) < parseInt(g.away_goal)) {
+				return 'text-left col-5 font-weight-bold';
+			} else {
+				return 'text-left col-5';
+			}
+		})
+		.html(function(g) { return '<span><img src="/img/' + g.away + '.png"></span>' + g.away; });
 		
-		.html(function(d) {
-			d.values.forEach
-			console.log(d);
-			return d.values[0].home 
-				+ ' '
-				+ d.values[0].home_goal 
-				+ ':'
-				+ d.values[0].away_goal
-				+ ' '
-				+ d.values[0].away;
-		});
+		// .html(function(d) {
+		// 	d.values.forEach
+		// 	console.log(d);
+		// 	return d.values[0].home 
+		// 		+ ' '
+		// 		+ d.values[0].home_goal 
+		// 		+ ':'
+		// 		+ d.values[0].away_goal
+		// 		+ ' '
+		// 		+ d.values[0].away;
+		// });
 		
 });
